bba618392fe5f15b8fe5632b98c9e6268a216ebb
eanhernandez <ean.hernandez@eanh.net>
2015-04-12 16:39:16 -0700

    refactoring

--- LtM.c
+++ LtM.c
@@ -3,52 +3,58 @@
 #include <stdlib.h>
 
 void sendMomentarySignal(int momentaryDelay);
+void turnOnLED(int LEDId);
+void turnOffLED(int LEDId);
+int latchIsPresent(int latchMonitor);
 
 int main( int argc, const char* argv[] )
 {
-        int state=0;		// tracks state of latching circuit, 1 = on and 0 = off
-        int momentaryDelay = 0;	// how long in ms to charge the relay which controls the momentary circuit
-        int loopDelay = 0;	// how long in ms to wait before checking for a latch
+        int momentaryDelay = 0;					// how long in ms to charge the relay which controls the momentary circuit
+        int loopDelay = 0;					// how long in ms to wait before checking for a latch
 
-	// checking for correct number of arguments
-        if (argc==3)
+        if (argc==3)						// checking for correct number of arguments
         {
                 momentaryDelay = atoi(argv[1]);
                 loopDelay = atoi(argv[2]);
         }
         else
         {
-                return 1;	// abort if there are no arguments
+                return 1;					// abort if there are no arguments
         }
 
-				//setting up pins...
-        wiringPiSetup () ;	// mandatory wiringpi setup
-	pinMode (4, INPUT) ;	// monitors for presence of latch
-	pinMode (0, OUTPUT) ;	// controls relay on momentary circuit
-	pinMode (2, OUTPUT) ; 	// powers "on" LED
-	pinMode (3, OUTPUT) ;	// powers "latch" LED
+                                                		// pin numbers...
+        int relayControl = 0;                   		// pin id where the relay is controlled
+        int powerLED = 2;                       		// pin id of the power LED
+        int latchLED = 3;                       		// pin id of the latch LED
+        int latchMonitor = 4;                   		// pin id where the latch is monitored
 
-	//turn on "on" light
-	digitalWrite (2, HIGH);
+								// setting up pins...
+        wiringPiSetup () ;					// mandatory wiringpi setup
+	pinMode (relayControl, OUTPUT) ;			// sets relayControl pin as an output
+	pinMode (powerLED, OUTPUT) ; 				// sets powerLED pin as an output
+	pinMode (latchLED, OUTPUT) ;				// sets latchLED pin as an output
+        pinMode (latchMonitor, INPUT) ;  			// sets latchMonitor pin as an input
 
-	//detect initial latch state
-	state = digitalRead(4);
+	turnOnLED(powerLED);					// turn on "power" LED
+
+	int latchState=0;                      			// tracks state of latching circuit, 1 = on and 0 = off
+	latchState = latchIsPresent(latchMonitor);		// detect initial latch state
 
 	for (;;)
 	{
-		if (state == 1 )				// if the state shows the latch as on
+		if (latchState)					// if the state shows the latch as on
 		{						//
-			digitalWrite (3, HIGH);			// turn on "latch" LED
+			turnOnLED(latchLED);			// turn on "latch" LED
 		}						//
 		else						//
 		{						//
-			digitalWrite (3, LOW);			// or if not, turn off "latch" LED 
+			turnOffLED(latchLED);			// or if not, turn off "latch" LED 
 		}
 
-		if (digitalRead(4)!=state)			// if there is change in latch: on to off, or off to on
+		if (latchIsPresent(latchMonitor)!=latchState)	// if there is change in latch: on to off, or off to on
 		{						//
 			sendMomentarySignal(momentaryDelay);	// fire momentary signal
-			state = state ^ 1;			// flip state to opposite of what it was
+			latchState ^= 1;			// flip state to opposite of what it was
 		}
 		
 		delay(loopDelay) ;				// wait a bit
@@ -56,13 +62,23 @@ int main( int argc, const char* argv[] )
 	return 0 ;
 }
 
-// sends the momentary signal to the privateeyepi relay board
-// which has it's own LED to show the relay is being charged
-// and thereby show that we are sending a momentary signal
-void sendMomentarySignal(int momentaryDelay)
+void sendMomentarySignal(int momentaryDelay)			// sends the momentary signal to the privateeyepi relay board
 {
         digitalWrite (0, HIGH) ;
         delay (momentaryDelay) ;
         digitalWrite (0,  LOW) ;
 }
+void turnOnLED(int LEDId)
+{
+	digitalWrite (LEDId, HIGH);
+}
 
+void turnOffLED(int LEDId)
+{
+	digitalWrite (LEDId, LOW);
+}
+
+int latchIsPresent(int latchMonitor)
+{
+	return digitalRead(latchMonitor);
+}
